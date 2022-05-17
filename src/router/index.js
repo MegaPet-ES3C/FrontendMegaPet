5966d154ec9d5fd6e015ac9aa20a0900403b1a17
2CTO <u201521691@upc.edu.pe>
2022-05-17 18:38:57 -0500

    fix: views deleted
    some unnecessary foldes were deleted

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../components/HomeView.vue";
+
 
 
 
@@ -7,11 +7,6 @@ import HomeView from "../components/HomeView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/home",
-      name: "home",
-      component: HomeView,
-    },
     {
       path: "/search",
       name: "search",
