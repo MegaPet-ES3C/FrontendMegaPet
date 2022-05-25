431b528f2edda85cdfb47a91c27755f13a6eebbc
XFernando43 <102422211+XFernando43@users.noreply.github.com>
2022-05-25 08:42:12 +0100

    fix: lenguage change.
    I translate the lenguage code.

--- src/user/pages/profile-section.component.vue
+++ src/user/pages/profile-section.component.vue
@@ -1,38 +1,33 @@
 <template>
-  <div class="card">
-    <pv-data-view :value="users" :layout="layout" :paginator="false" :rows="9">
+  <body class="card">
 
-      <template #grid="slotUsers">
-
-        <!-- nuevo -->
-
-        <div class="perfil">
+        <div class="Profile">
           <div class="card_header">
-            <div class="imagen">
-              <img :src='slotUsers.data.image' style="width:5rem"/>
+            <div class="Image">
+              <img :src='user.image' style="width:5rem"/>
             </div>
             <div>
-              <div class="nombre"> {{slotUsers.data.name}} </div>
-              <div class="descripcion"> {{slotUsers.data.lastName}}</div>
-              <div class="elementos">
+              <div class="UserName"> {{user.name}} </div>
+              <div class="UserLastname"> {{user.lastName}}</div>
+              <div class="Items">
                 <a href="" class="pi pi-facebook"></a>
                 <a href="" class="pi pi-twitter"></a>
                 <a href="" class="pi pi-github"></a>
                 <a href="" class="pi pi-youtube"></a>
               </div>
               <div>
-                <a href="" class="contact-me">Contactame</a>
+                <a href="" class="Message-me">My Messages</a>
               </div>
               <div class="card_footer">
                 <div class="numbers">
                   <div class="item">
-                    <span>120</span> Seguidos
+                    <span>120</span> Follows
                   </div>
                   <div class="item">
-                    <span>127</span> Seguidores
+                    <span>127</span> Followers
                   </div>
                   <div class="item">
-                    <span>220</span> Publicaciones
+                    <span>220</span> Posts
                   </div>
                 </div>
               </div>
@@ -40,11 +35,7 @@
           </div>
         </div>
 
-
-      </template>
-
-    </pv-data-view>
-  </div>
+  </body>
 </template>
 
 <script>
@@ -54,16 +45,14 @@ export default {
   name: "profile-section.component",
   data() {
     return {
-      users: [],
-      user: {},
-      userService: null,
-      layout: "grid",
+      user: null,
+      userService: null
     };
   },
   created() {
     this.userService = new UsersApiService();
-    this.userService.getAll().then((response) => {
-      this.users = response.data;
+    this.userService.getById(1).then((response) => {
+      this.user = response.data;
       console.log("created");
     });
   },
@@ -79,15 +68,15 @@ export default {
   box-sizing: border-box;
   text-decoration: none;
 }
-.body{
+body{
   heigh: 100vh;
-  background: url(../src/assets/logo.svg);
+  background: #ffffff;
   background-size: cover;
   display: flex;
   align-items:center;
   justify-content:center;
 }
-.perfil{
+.Profile{
   width:400px;
   text-align: center;
   border-radius: 8px;
@@ -97,7 +86,7 @@ export default {
   background: #2c3e50;
   padding: 60px 40px;
 }
-.imagen{
+.Image{
   image-resolution: unset;
   display: inline-block;
   padding: 8px;
@@ -123,37 +112,37 @@ export default {
     background-position: left top;
   }
 }
-.imagen img{
+.Image img{
   display: block;
   width: 100px;
   heigh: 100px;
   border-radius: 50%;
 }
-.nombre{
+.UserName{
   color:#ffffff;
   font-size:28px;
   font-weight: 600;
   margin: 10px 0;
 }
-.descripcion{
+.UserLastname{
   font-size: 18px;
-  color: #e66767;
+  color: #ffffff;
 }
-.elementos{
+.Items{
   display: flex;
   justify-content: center;
   margin: 20px 0;
 }
-.elementos a{
+.Items a{
   color:#ffffff;
   width: 56px;
   font-size: 22px;
   transition: .3s linear;
 }
-.elementos a:hover {
+.Items a:hover {
   color: #e66767;
 }
-.contact-me{
+.Message-me{
   display: inline-block;
   padding: 12px 50px;
   color: #e66767;
@@ -162,7 +151,7 @@ export default {
   margin-top: 16px;
   transition: .3s linear;
 }
-.contact-me:hover{
+.Message-me:hover{
   background: #e66767;
   color: #fff;
 }
