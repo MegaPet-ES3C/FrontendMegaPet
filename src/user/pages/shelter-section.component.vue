c9745a18d585a5345807d0be72271b3d1369623e
diegoosorioupc <diegoosorioupc@gmail.com>
2022-05-27 02:00:46 -0500

    Add: shelter-section.component.vue

--- src/user/pages/shelter-section.component.vue
+++ src/user/pages/shelter-section.component.vue
@@ -1,13 +1,73 @@
 <template>
-<div>Shelter Section works!</div>
+
+  <div class="container">
+    <div class="cards">
+      <template v-for="shelter in shelters">
+        <pv-card>
+          <template #header>
+            <img :src='shelter.image' />
+          </template>
+          <template #title>
+            {{shelter.name}}
+          </template>
+          <template #content>
+            {{shelter.description}}
+          </template>
+        </pv-card>
+      </template>
+    </div>
+  </div>
+
 </template>
 
 <script>
+import { SheltersApiService } from "../services/shelters-api.service";
+
 export default {
-  name: "shelter-section.component"
+  name: "shelter-section.component",
+  data() {
+    return {
+      sheltersService: null,
+      shelters: [],
+      shelterDialog: false,
+      deleteShelterDialog: false,
+      deleteSheltersDialog: false,
+      shelter: {},
+      filters:{},
+      submitted: false,
+    };
+  },
+
+  created(){
+    this.sheltersService = new SheltersApiService();
+    this.sheltersService.getAll().then((response) => {
+      this.shelters = response.data;
+      console.log("created");
+    });
+  }
+
 }
+
 </script>
 
 <style scoped>
+.cards {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin:100px 0px;
+  max-width: 1200px;
 
+}
+
+.container{
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+img{
+  height: 200px;
+  object-fit: contain;
+  background-color: #EAEAEA;
+}
 </style>
