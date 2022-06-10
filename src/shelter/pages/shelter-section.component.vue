1bd35bccd0afcfff9e1ed5d8b17047bd3ca02749
diegoosorioupc <84487640+diegoosorioupc@users.noreply.github.com>
2022-06-10 10:21:33 -0500

    add: create new shelter

--- src/shelter/pages/shelter-section.component.vue
+++ src/shelter/pages/shelter-section.component.vue
@@ -1,6 +1,39 @@
 <template>
+  <pv-button label="Nuevo" icon="pi pi-plus" @click="displayBasic = true"/>
+  <pv-dialog  v-model:visible="displayBasic" >
+    <template #header>
+      <h3>Header</h3>
+    </template>
 
+    <form @submit="newShelter">
+      <label>Name</label> <br>
+      <pv-input-text type="text" id="name" v-model.trim="shelter.name"  /> <br><br>
+      <label>Description</label> <br>
+      <pv-text-area v-model.trim="shelter.description" /><br><br>
+      <label>Link</label> <br>
+      <pv-input-text type="text" id="url" v-model.trim="shelter.url" /><br><br>
+      <label>Phone</label> <br>
+      <pv-input-text type="text" id="phone" v-model.trim="shelter.phone" /><br><br>
+      <label>Location</label> <br>
+      <pv-input-text type="text" id="location" v-model.trim="shelter.location" /><br><br>
+      <label>Address</label> <br>
+      <pv-input-text type="text" id="adress" v-model.trim="shelter.address" /><br><br>
+      <label>URL image</label> <br>
+      <pv-input-text type="text" id="image" v-model.trim="shelter.image" /><br><br>
+
+      <pv-button type="submit" label="Submit" icon="pi pi-check" autofocus />
+    </form>
+
+    <template #footer>
+      <pv-button label="Cancelar" icon="pi pi-times" @click="displayBasic = false"/>
+
+    </template>
+
+  </pv-dialog>
   <div class="container">
+
+
+
     <div class="cards">
       <template v-for="shelter in shelters">
         <pv-card>
@@ -32,7 +65,11 @@ import { SheltersApiService } from "../services/shelters-api.service";
 export default {
   name: "shelter-section.component",
   data() {
+
+
+
     return {
+      displayBasic: false,
       sheltersService: null,
       shelters: [],
       shelterDialog: false,
@@ -50,6 +87,36 @@ export default {
       this.shelters = response.data;
       console.log("created");
     });
+  },
+
+
+  methods:{
+    getStorableShelter(displayableShelter) {
+      return {
+        id: displayableShelter.id,
+        name: displayableShelter.name,
+        description: displayableShelter.description,
+        url: displayableShelter.url,
+        image: displayableShelter.image,
+        phone: displayableShelter.phone,
+        location: displayableShelter.location,
+        address: displayableShelter.address,
+      }
+    },
+
+    newShelter(){
+      this.submitted = true;
+      this.shelter.id = 0;
+      console.log(this.shelter);
+      this.shelter = this.getStorableShelter(this.shelter);
+      this.sheltersService.create(this.shelter).then((response) => {
+        this.shelter.push(this.shelter);
+        console.log(response);
+      });
+
+      this.shelter = {};
+
+    }
   }
 
 }
